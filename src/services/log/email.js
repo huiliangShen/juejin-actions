a7f62ed0e735be57dc7728061c34916bf9959e46
banruo <shl@dataqin.com>
2022-01-25 18:36:42 +0800

    email

--- src/services/log/email.js
+++ src/services/log/email.js
@@ -1,9 +1,55 @@
-export default class LogService {
-    isValidate = false
+import {createTransport} from 'nodemailer'
+import {email} from "../../config";
+import LogService from "./log-service";
 
+class EmailService extends LogService {
     constructor() {
+        super();
+        this.isValidate = email.host && email.pass && email.port && email.to
     }
-    async log(...data) {}
-    async error(...data) {}
-    async sendMessage(...data) {}
+
+    async log(...data) {
+        console.log('email log:', ...data)
+    }
+
+    async error(...data) {
+        console.log('email error:', ...data)
+    }
+
+    async sendMessage(content) {
+        if (!this.isValidate) {
+            return
+        }
+
+        const transporter = await createTransport({
+            host: email.host,
+            port: Number(email.port),
+            secure: Number(email.port) === 465,
+            auth: {
+                user: email.user,
+                pass: email.pass
+            }
+        })
+
+        await transporter.sendMail({
+            from: `掘金<${email.user}>`,
+            to: email.to,
+            subject: '掘金自动签到',
+            html: this.assembledText(content)
+        })
+    }
+
+    assembledText(content) {
+        if (Array.isArray(content)) {
+            const html = [];
+            content.forEach((text) => {
+                html.push(`<p>${text}</p>`);
+            });
+            return `<div>${html.join('')}</div>`
+        } else if (typeof content === 'string') {
+            return content;
+        }
+    }
 }
+
+export default new EmailService();
